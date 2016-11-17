d2edbfd515aab8ffd7368911743a4bbc0d6da42a
krstics <cobikrstic@gmail.com>
2016-11-17 22:24:08 +0100

    Update Constants.java

--- app/src/main/java/com/krstics/watchreminder/Helpers/Constants.java
+++ app/src/main/java/com/krstics/watchreminder/Helpers/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
         public static final String BASE_URL_TVDB = "http://thetvdb.com/api/";
         public static final String EPISODE_BY_AIR_DATE = "GetEpisodeByAirDate.php";
         public static final String BANNERS_URL = "http://thetvdb.com/banners/";
-        public static final String API_KEY = "07C679B9A55DD6A4";
+        public static final String API_KEY = "";
         public static final String TYPE_SERIES = "/series/";
         public static final String TYPE_EPISODES = "/episodes/";
         public static final String ALL = "/all";
